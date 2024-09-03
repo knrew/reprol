16b491a90ea2a58438b3cddb3543fa2b0eebe52e
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2024-09-03 16:14:20 +0900

    add test for gcd/lcm

--- src/math.rs
+++ src/math.rs
@@ -19,3 +19,48 @@ fn abs<T: Integer>(n: T) -> T {
         n
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::math::{gcd, lcm};
+
+    #[test]
+    fn test_gcd() {
+        assert_eq!(gcd(48, 18), 6);
+        assert_eq!(gcd(54, 24), 6);
+        assert_eq!(gcd(101, 103), 1);
+        assert_eq!(gcd(0, 10), 10);
+        assert_eq!(gcd(10, 0), 10);
+        assert_eq!(gcd(0, 0), 0);
+        assert_eq!(gcd(48u32, 18u32), 6);
+        assert_eq!(gcd(54u64, 24u64), 6);
+        assert_eq!(gcd(-48, -18), 6);
+        assert_eq!(gcd(-54, 24), 6);
+        assert_eq!(gcd(-101, -103), 1);
+        assert_eq!(
+            gcd(1_000_000_000_000_000_000u128, 500_000_000_000_000_000u128),
+            500_000_000_000_000_000u128
+        );
+        assert_eq!(gcd(42, 42), 42);
+        assert_eq!(gcd(-42, -42), 42);
+    }
+
+    #[test]
+    fn test_lcm() {
+        assert_eq!(lcm(4, 5), 20);
+        assert_eq!(lcm(6, 8), 24);
+        assert_eq!(lcm(7, 3), 21);
+        assert_eq!(lcm(10, 15), 30);
+        assert_eq!(lcm(7u32, 3u32), 21);
+        assert_eq!(lcm(9u64, 6u64), 18);
+        assert_eq!(lcm(-4, 5), 20);
+        assert_eq!(lcm(-6, -8), 24);
+        assert_eq!(lcm(-7, 3), 21);
+        assert_eq!(
+            lcm(1_000_000_000_000_000_000u128, 500_000_000_000_000_000u128),
+            1_000_000_000_000_000_000u128
+        );
+        assert_eq!(lcm(42, 42), 42);
+        assert_eq!(lcm(-42, -42), 42);
+    }
+}
