8866d9a08358a8b390a0351ff038b9fab9766360
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2024-11-05 06:11:13 +0900

    update

--- snippets/neighbors.rs
+++ snippets/neighbors.rs
@@ -1,44 +1,17 @@
 //! グリッド上の隣接するマスを列挙する
 
+const D: [[usize; 2]; 4] = [
+    [1, 0],
+    [0, 1],
+    [1usize.wrapping_neg(), 0],
+    [0, 1usize.wrapping_neg()],
+];
+
 fn collect_neighbors(
     &(h, w): &(usize, usize),
     &(i, j): &(usize, usize),
 ) -> impl Iterator<Item = (usize, usize)> {
-    const D: [(usize, usize); 4] = [
-        (1, 0),
-        (0, 1),
-        (1usize.wrapping_neg(), 0),
-        (0, 1usize.wrapping_neg()),
-    ];
-    (0..4)
-        .map(move |k| (i.wrapping_add(D[k].0), j.wrapping_add(D[k].1)))
+    D.iter()
+        .map(move |&[di, dj]| (i.wrapping_add(di), j.wrapping_add(dj)))
         .filter(move |&(i, j)| i < h && j < w)
-        .map(move |(i, j)| (i, j))
-}
-
-fn func() {
-    // vec
-    let collect_neighbors = |(i, j): (usize, usize)| -> Vec<(usize, usize)> {
-        const D: [(usize, usize); 4] = [
-            (1, 0),
-            (0, 1),
-            (1usize.wrapping_neg(), 0),
-            (0, 1usize.wrapping_neg()),
-        ];
-        (0..4)
-            .map(|k| (i.wrapping_add(D[i].0), j.wrapping_add(D[i].1)))
-            .filter(|&(i, j)| i < h && j < w)
-            .collect()
-    };
-
-    // iterator
-    const D: [(usize, usize); 4] = [
-        (1, 0),
-        (0, 1),
-        (1usize.wrapping_neg(), 0),
-        (0, 1usize.wrapping_neg()),
-    ];
-    let iter = (0..4)
-        .map(|k| (i.wrapping_add(D[i].0), j.wrapping_add(D[i].1)))
-        .filter(move |&(i, j)| i < h && j < w);
 }
--- src/math/radix.rs
+++ src/math/radix.rs
@@ -1,15 +1,16 @@
 pub trait ToRadix {
-    /// 10進法の非負整数をbase進法に変換する
-    /// base^iの係数がres[i]に格納される
-    /// TODO: resの格納順は逆のほうが自然？
+    /// 10進法の非負整数をn進法に変換する
+    /// 返り値の配列のi番目の要素は変換後の桁数をdとしてbase^{d-i}の係数が格納される
     fn to_radix(self, base: Self) -> Vec<u8>;
 }
 
 macro_rules! impl_integer {
     ($($ty:ident),*) => {$(
-
         impl ToRadix for $ty {
             fn to_radix(self, base: Self) -> Vec<u8> {
+                if self == 0 {
+                    return vec![0];
+                }
                 let mut n = self;
                 let mut res = vec![];
                 while n > 0 {
@@ -17,10 +18,10 @@ macro_rules! impl_integer {
                     res.push(x);
                     n /= base;
                 }
+                res.reverse();
                 res
             }
         }
-
     )*};
 }
 
