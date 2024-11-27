0512e6f2107789c8f5b4c67ba6aa78ffc68b3977
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2024-11-28 06:07:26 +0900

    refactor

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,97 +1,164 @@
-/// NxM行列を時計回りに90度回転させる
-/// 処理後の行列はMxN行列になる
-pub fn rotate_clockwise<T>(m: &[Vec<T>]) -> Vec<Vec<T>>
+pub trait Matrix {
+    type Output;
+
+    /// NxM行列を時計回りに90度回転させる
+    /// 処理後の行列はMxN行列になる
+    fn rotate_clockwise(&self) -> Self::Output;
+
+    /// NxM行列を反時計回りに90度回転させる
+    /// 処理後の行列はMxN行列になる
+    fn rotate_anticlockwise(&self) -> Self::Output;
+
+    /// 転置行列を計算する
+    fn transpose(&self) -> Self::Output;
+}
+
+impl<T> Matrix for Vec<Vec<T>>
 where
     T: Clone,
 {
-    debug_assert!(!m.is_empty());
-    let mut rotated = vec![vec![m[0][0].clone(); m.len()]; m[0].len()];
-    for i in 0..m.len() {
-        for j in 0..m[i].len() {
-            rotated[j][m.len() - 1 - i] = m[i][j].clone();
+    type Output = Vec<Vec<T>>;
+    fn rotate_clockwise(&self) -> Self::Output {
+        rotate_clockwise(self)
+    }
+    fn rotate_anticlockwise(&self) -> Self::Output {
+        rotate_anticlockwise(self)
+    }
+    fn transpose(&self) -> Self::Output {
+        transpose(self)
+    }
+}
+
+impl<T> Matrix for &[Vec<T>]
+where
+    T: Clone,
+{
+    type Output = Vec<Vec<T>>;
+    fn rotate_clockwise(&self) -> Self::Output {
+        rotate_clockwise(self)
+    }
+    fn rotate_anticlockwise(&self) -> Self::Output {
+        rotate_anticlockwise(self)
+    }
+    fn transpose(&self) -> Self::Output {
+        transpose(self)
+    }
+}
+
+/// NxM行列を時計回りに90度回転させる
+/// 処理後の行列はMxN行列になる
+fn rotate_clockwise<T>(m: &[Vec<T>]) -> Vec<Vec<T>>
+where
+    T: Clone,
+{
+    assert!(!m.is_empty());
+
+    let h = m[0].len();
+    let w = m.len();
+
+    let mut res = vec![vec![m[0][0].clone(); w]; h];
+
+    for i in 0..w {
+        for j in 0..h {
+            res[j][w - 1 - i] = m[i][j].clone();
         }
     }
-    rotated
+
+    res
 }
 
 /// NxM行列を反時計回りに90度回転させる
 /// 処理後の行列はMxN行列になる
-pub fn rotate_anticlockwise<T>(m: &[Vec<T>]) -> Vec<Vec<T>>
+fn rotate_anticlockwise<T>(m: &[Vec<T>]) -> Vec<Vec<T>>
 where
     T: Clone,
 {
-    debug_assert!(!m.is_empty());
-    let mut rotated = vec![vec![m[0][0].clone(); m.len()]; m[0].len()];
-    for i in 0..m.len() {
-        for j in 0..m[i].len() {
-            rotated[m[i].len() - 1 - j][i] = m[i][j].clone();
+    assert!(!m.is_empty());
+
+    let h = m[0].len();
+    let w = m.len();
+
+    let mut res = vec![vec![m[0][0].clone(); w]; h];
+
+    for i in 0..w {
+        for j in 0..h {
+            res[h - 1 - j][i] = m[i][j].clone();
         }
     }
-    rotated
+
+    res
 }
 
 /// 転置行列を計算する
-pub fn transpose<T>(m: &[Vec<T>]) -> Vec<Vec<T>>
+fn transpose<T>(m: &[Vec<T>]) -> Vec<Vec<T>>
 where
     T: Clone,
 {
     if m.is_empty() {
         return vec![];
     }
-    debug_assert!(!m[0].is_empty());
+
+    assert!(!m[0].is_empty());
+
     let nrows = m.len();
     let ncols = m[0].len();
-    let mut transposed = vec![vec![m[0][0].clone(); nrows]; ncols];
+
+    let mut res = vec![vec![m[0][0].clone(); nrows]; ncols];
+
     for i in 0..nrows {
         for j in 0..ncols {
-            transposed[j][i] = m[i][j].clone();
+            res[j][i] = m[i][j].clone();
         }
     }
-    transposed
+
+    res
 }
 
 // TODO: 正方行列以外のテストを書く
 #[cfg(test)]
 mod tests {
-    use super::{rotate_anticlockwise, rotate_clockwise, transpose};
+    use super::Matrix;
 
     #[test]
-    fn test_rotate() {
+    fn test_rotate_clockwise() {
         let m = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let rotated = vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3]];
+        assert_eq!(m.rotate_clockwise(), rotated);
+    }
 
-        let clockwise_rotated = vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3]];
-        assert_eq!(rotate_clockwise(&m), clockwise_rotated);
-
-        let anticlockwise_rotated = vec![vec![3, 6, 9], vec![2, 5, 8], vec![1, 4, 7]];
-        assert_eq!(rotate_anticlockwise(&m), anticlockwise_rotated);
+    #[test]
+    fn test_rotate_anticlockwise() {
+        let m = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let rotated = vec![vec![3, 6, 9], vec![2, 5, 8], vec![1, 4, 7]];
+        assert_eq!(m.rotate_anticlockwise(), rotated);
     }
 
     #[test]
     fn test_transpose() {
         let m = vec![vec![1, 2, 3], vec![4, 5, 6]];
         let transposed = vec![vec![1, 4], vec![2, 5], vec![3, 6]];
-        assert_eq!(transpose(&m), transposed);
+        assert_eq!(m.transpose(), transposed);
 
         let m = vec![vec![1, 2], vec![3, 4], vec![5, 6]];
         let transposed = vec![vec![1, 3, 5], vec![2, 4, 6]];
-        assert_eq!(transpose(&m), transposed);
+        assert_eq!(m.transpose(), transposed);
 
         let m = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
         let transposed = vec![vec![1, 4, 7], vec![2, 5, 8], vec![3, 6, 9]];
-        assert_eq!(transpose(&m), transposed);
+        assert_eq!(m.transpose(), transposed);
 
         let m = vec![vec![1, 2, 3]];
         let transposed = vec![vec![1], vec![2], vec![3]];
-        assert_eq!(transpose(&m), transposed);
+        assert_eq!(m.transpose(), transposed);
 
         let m = vec![vec![1], vec![2], vec![3]];
         let transposed = vec![vec![1, 2, 3]];
-        assert_eq!(transpose(&m), transposed);
+        assert_eq!(m.transpose(), transposed);
 
         let m = vec![vec![6]];
-        assert_eq!(transpose(&m), m);
+        assert_eq!(m.transpose(), m);
 
         let m: Vec<Vec<i32>> = vec![];
-        assert_eq!(transpose(&m), m);
+        assert_eq!(m.transpose(), m);
     }
 }
--- src/rle.rs
+++ src/rle.rs
@@ -1,20 +1,61 @@
 /// ランレングス圧縮(run-length encodeing)
-pub fn rle<T>(s: &[T]) -> Vec<(T, usize)>
-where
-    T: Copy + PartialEq,
-{
-    let mut res = vec![];
+/// TODO: イテレータにする
+pub trait RunLengthEncoding {
+    type Output;
 
-    for &c in s {
-        match res.last_mut() {
-            Some((last_c, count)) if *last_c == c => {
-                *count += 1;
-            }
-            _ => {
-                res.push((c, 1));
+    /// ランレングス圧縮を行う
+    fn rle(&self) -> Self::Output;
+}
+
+impl<T> RunLengthEncoding for Vec<T>
+where
+    T: Clone + PartialEq,
+{
+    type Output = Vec<(T, usize)>;
+
+    fn rle(&self) -> Self::Output {
+        let mut res = vec![];
+
+        for e in self {
+            match res.last_mut() {
+                Some((last_e, count)) if last_e == e => {
+                    *count += 1;
+                }
+                _ => {
+                    res.push((e.clone(), 1));
+                }
             }
         }
-    }
 
-    res
+        res
+    }
+}
+
+impl<T> RunLengthEncoding for &[T]
+where
+    T: Clone + PartialEq,
+{
+    type Output = Vec<(T, usize)>;
+
+    fn rle(&self) -> Self::Output {
+        self.to_vec().rle()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::RunLengthEncoding;
+
+    #[test]
+    fn test_rle() {
+        let s = "aaabbbcccddeee".chars().collect::<Vec<char>>();
+        let expected = vec![('a', 3), ('b', 3), ('c', 3), ('d', 2), ('e', 3)];
+        assert_eq!(s.rle(), expected);
+
+        let s = "abcde".chars().collect::<Vec<char>>();
+        let expected = vec![('a', 1), ('b', 1), ('c', 1), ('d', 1), ('e', 1)];
+        assert_eq!(s.rle(), expected);
+
+        assert_eq!(Vec::<char>::new().rle(), Vec::new());
+    }
 }
