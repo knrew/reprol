1dab68aa1e4005338ffa5600e87792c8b9206fda
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2024-11-28 04:43:27 +0900

    add interval_set

---
+++ src/interval_set.rs
@@ -0,0 +1,70 @@
+//! 区間をsetで管理するやつ
+
+use std::collections::BTreeSet;
+
+pub struct IntervalSet<T>(BTreeSet<(T, T)>);
+
+impl<T> IntervalSet<T>
+where
+    T: Integer,
+{
+    pub fn new() -> Self {
+        Self(BTreeSet::new())
+    }
+
+    pub fn insert(&mut self, mut l: T, mut r: T) {
+        assert!(l <= r);
+
+        if let Some(&(ll, rr)) = self.0.range(..=(l, T::sup())).max() {
+            if l <= rr {
+                l = ll;
+            }
+        }
+
+        if let Some(&(_, rr)) = self.0.range(..=(r, T::sup())).max() {
+            if r <= rr {
+                r = rr;
+            }
+        }
+
+        let mut sub = vec![];
+
+        for &(ll, rr) in self.0.range((l, T::inf())..=(r, T::sup())) {
+            sub.push((ll, rr));
+        }
+
+        for x in sub {
+            self.0.remove(&x);
+        }
+
+        self.0.insert((l, r));
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &(T, T)> {
+        self.0.iter()
+    }
+
+    pub fn into_iter(self) -> impl IntoIterator<Item = (T, T)> {
+        self.0.into_iter()
+    }
+}
+
+pub trait Integer: Ord + Copy {
+    fn inf() -> Self;
+    fn sup() -> Self;
+}
+
+macro_rules! impl_integer {
+    ($($ty:ident),*) => {$(
+        impl Integer for $ty {
+            fn inf() -> Self {
+                $ty::MIN
+            }
+            fn sup() -> Self {
+                $ty::MAX
+            }
+        }
+    )*};
+}
+
+impl_integer! { u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize }
