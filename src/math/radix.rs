c180d7c0d73d96af92050654f81561f1b2d85da6
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2024-12-17 18:37:24 +0900

    fix from_radix

--- src/math/radix.rs
+++ src/math/radix.rs
@@ -8,7 +8,7 @@ pub trait FromRadix {
     type Output;
 
     /// n進法の整数を10進法に変換する
-    fn from_radix_unchecked(&self, n: u32) -> Self::Output;
+    fn from_radix(&self, n: u32) -> Self::Output;
 }
 
 macro_rules! impl_integer {
@@ -34,10 +34,36 @@ macro_rules! impl_integer {
 
 impl_integer!(u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize);
 
+impl FromRadix for String {
+    type Output = u64;
+
+    fn from_radix(&self, n: u32) -> Self::Output {
+        u64::from_str_radix(self, n).unwrap()
+    }
+}
+
 impl FromRadix for &str {
     type Output = u64;
 
-    fn from_radix_unchecked(&self, n: u32) -> Self::Output {
+    fn from_radix(&self, n: u32) -> Self::Output {
         u64::from_str_radix(self, n).unwrap()
+    }
+}
+
+/// ToRadixの逆
+/// n進法の数(桁数d)のn^iの位の数が(d-i)番目の要素として格納されている配列に対して
+/// 10進法に変換した値を返す
+/// Bytesをそのまま突っ込むものではないので注意
+impl FromRadix for Vec<u32> {
+    type Output = u64;
+    fn from_radix(&self, n: u32) -> Self::Output {
+        let n = n as u64;
+        let mut res = 0;
+        let mut base = 1;
+        for &e in self.iter().rev() {
+            res += e as u64 * base;
+            base *= n;
+        }
+        res
     }
 }
