7607897010f6346a603632b5e8b6731dfb334674
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2025-01-24 16:06:38 +0900

    add io snippet

---
+++ snippets/io.rs
@@ -0,0 +1,23 @@
+use std::io::{Read, Write};
+
+fn main() {
+    let s = {
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        s
+    };
+    let mut stdin = s.split_whitespace();
+    let mut stdout = std::io::BufWriter::new(std::io::stdout().lock());
+
+    let n = read::<usize>(&mut stdin);
+    writeln!(stdout, "{}", n).ok();
+}
+
+#[inline]
+fn read<T>(iter: &mut std::str::SplitWhitespace<'_>) -> T
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    iter.next().unwrap().parse::<T>().unwrap()
+}
