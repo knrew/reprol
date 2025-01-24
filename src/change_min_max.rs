9bfb45b50f5651cc2ffb7df8265513a968fb4a2d
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2025-01-24 16:07:03 +0900

    add chmin/chmax for option

--- src/change_min_max.rs
+++ src/change_min_max.rs
@@ -7,21 +7,55 @@ impl<T> ChangeMinMax for T
 where
     T: PartialOrd,
 {
-    fn change_min(&mut self, value: T) -> bool {
-        if value < *self {
-            *self = value;
+    fn change_min(&mut self, rhs: T) -> bool {
+        if *self > rhs {
+            *self = rhs;
             true
         } else {
             false
         }
     }
 
-    fn change_max(&mut self, value: T) -> bool {
-        if *self < value {
-            *self = value;
+    fn change_max(&mut self, rhs: T) -> bool {
+        if *self < rhs {
+            *self = rhs;
             true
         } else {
             false
+        }
+    }
+}
+
+/// Option型に対してNoneなら代入，Someなら最小値(最大値)を更新する
+pub trait ChangeMinMaxOrSet {
+    type Item;
+    fn change_min_or_set(&mut self, value: Self::Item) -> bool;
+    fn change_max_or_set(&mut self, value: Self::Item) -> bool;
+}
+
+impl<T> ChangeMinMaxOrSet for Option<T>
+where
+    T: PartialOrd,
+{
+    type Item = T;
+
+    fn change_min_or_set(&mut self, rhs: T) -> bool {
+        match self {
+            Some(current) if *current < rhs => false,
+            _ => {
+                *self = Some(rhs);
+                true
+            }
+        }
+    }
+
+    fn change_max_or_set(&mut self, rhs: Self::Item) -> bool {
+        match self {
+            Some(current) if *current > rhs => false,
+            _ => {
+                *self = Some(rhs);
+                true
+            }
         }
     }
 }
