0d979214a548875a59436e2b7906fd605f3ff756
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2025-01-29 03:47:55 +0900

    add testcases for radix

--- src/math/radix.rs
+++ src/math/radix.rs
@@ -32,7 +32,7 @@ macro_rules! impl_integer {
     )*};
 }
 
-impl_integer!(u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize);
+impl_integer! { u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize }
 
 impl FromRadix for String {
     type Output = u64;
@@ -65,5 +65,42 @@ impl FromRadix for Vec<u32> {
             base *= n;
         }
         res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{FromRadix, ToRadix};
+
+    #[test]
+    fn test_to_radix() {
+        let x = 697;
+        assert_eq!(x.to_radix(10), vec![6, 9, 7]);
+        assert_eq!(x.to_radix(2), vec![1, 0, 1, 0, 1, 1, 1, 0, 0, 1]);
+        assert_eq!(x.to_radix(8), vec![1, 2, 7, 1]);
+        assert_eq!(x.to_radix(16), vec![2, 11, 9]);
+        assert_eq!(x.to_radix(5), vec![1, 0, 2, 4, 2]);
+        let x = 1234;
+        assert_eq!(x.to_radix(10), vec![1, 2, 3, 4]);
+        assert_eq!(x.to_radix(2), vec![1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0]);
+        assert_eq!(x.to_radix(8), vec![2, 3, 2, 2]);
+        assert_eq!(x.to_radix(16), vec![4, 13, 2]);
+        assert_eq!(x.to_radix(5), vec![1, 4, 4, 1, 4]);
+    }
+
+    #[test]
+    fn test_from_radix() {
+        assert_eq!(vec![6, 9, 7].from_radix(10), 697);
+        assert_eq!(vec![1, 0, 1, 0, 1, 1, 1, 0, 0, 1].from_radix(2), 697);
+        assert_eq!(vec![1, 2, 7, 1].from_radix(8), 697);
+        assert_eq!(vec![2, 11, 9].from_radix(16), 697);
+        assert_eq!(vec![1, 0, 2, 4, 2].from_radix(5), 697);
+        assert_eq!(vec![1, 2, 3, 4].from_radix(10), 1234);
+        assert_eq!(vec![1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0].from_radix(2), 1234);
+        assert_eq!(vec![2, 3, 2, 2].from_radix(8), 1234);
+        assert_eq!(vec![4, 13, 2].from_radix(16), 1234);
+        assert_eq!(vec![1, 4, 4, 1, 4].from_radix(5), 1234);
+        assert_eq!("1271".from_radix(8), 697);
+        assert_eq!("4d2".from_radix(16), 1234);
     }
 }
