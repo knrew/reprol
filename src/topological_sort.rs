581a664b481b509c2e126114cd8a876d3190fd9d
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2025-02-11 18:49:46 +0900

    update topological_sort

--- src/topological_sort.rs
+++ src/topological_sort.rs
@@ -1,72 +1,128 @@
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, VecDeque},
+};
 
-pub trait TopologicalSort {
-    /// トポロジカルソート
-    /// 入力は隣接リスト表現のグラフ
-    /// 辞書順最小の頂点番号配列を返す
-    /// グラフが閉路を含む場合はNoneを返す
-    fn topological_sort(&self) -> Option<Vec<usize>>;
+pub struct ToplogicalSort {
+    /// トポロジカル順序
+    order: Option<Vec<usize>>,
+
+    /// トポロジカル順序が一意かどうか
+    is_unique: bool,
 }
 
-impl TopologicalSort for Vec<Vec<usize>> {
-    fn topological_sort(&self) -> Option<Vec<usize>> {
-        topological_sort(self)
-    }
-}
+impl ToplogicalSort {
+    /// 隣接リスト表現のグラフをトポロジカルソートする
+    /// 複数のトポロジカル順序が存在する場合は辞書順最小のものを求める
+    pub fn new(graph: &[Vec<usize>]) -> Self {
+        let mut indegrees = vec![0; graph.len()];
 
-impl TopologicalSort for [Vec<usize>] {
-    fn topological_sort(&self) -> Option<Vec<usize>> {
-        topological_sort(self)
-    }
-}
-
-/// トポロジカルソート
-/// 入力は隣接リスト表現のグラフ
-/// 辞書順最小の頂点番号配列を返す
-/// グラフが閉路を含む場合はNoneを返す
-fn topological_sort(graph: &[Vec<usize>]) -> Option<Vec<usize>> {
-    let mut degrees = vec![0; graph.len()];
-
-    for v in graph {
-        for &nv in v {
-            degrees[nv] += 1;
-        }
-    }
-
-    let mut heap = BinaryHeap::new();
-
-    for i in 0..graph.len() {
-        if degrees[i] == 0 {
-            heap.push(Reverse(i));
-        }
-    }
-
-    let mut res = vec![];
-
-    while let Some(Reverse(v)) = heap.pop() {
-        res.push(v);
-
-        for &nv in &graph[v] {
-            degrees[nv] -= 1;
-            if degrees[nv] == 0 {
-                heap.push(Reverse(nv));
+        for v in graph {
+            for &nv in v {
+                indegrees[nv] += 1;
             }
         }
+
+        let mut heap = BinaryHeap::new();
+
+        for i in 0..graph.len() {
+            if indegrees[i] == 0 {
+                heap.push(Reverse(i));
+            }
+        }
+
+        let mut order = vec![];
+        let mut is_unique = true;
+
+        while let Some(Reverse(v)) = heap.pop() {
+            if heap.len() >= 1 {
+                is_unique = false;
+            }
+            order.push(v);
+
+            for &nv in &graph[v] {
+                indegrees[nv] -= 1;
+                if indegrees[nv] == 0 {
+                    heap.push(Reverse(nv));
+                }
+            }
+        }
+
+        let order = if order.len() == graph.len() {
+            Some(order)
+        } else {
+            None
+        };
+
+        Self { order, is_unique }
     }
 
-    if res.len() == graph.len() {
-        Some(res)
-    } else {
-        None
+    /// 隣接リスト表現のグラフをトポロジカルソートする
+    /// 複数のトポロジカル順序が存在する場合でも辞書順を保証しない
+    pub fn new_without_lexical_order(graph: &[Vec<usize>]) -> Self {
+        let mut indegrees = vec![0; graph.len()];
+
+        for v in graph {
+            for &nv in v {
+                indegrees[nv] += 1;
+            }
+        }
+
+        let mut que = VecDeque::new();
+
+        for i in 0..graph.len() {
+            if indegrees[i] == 0 {
+                que.push_back(i);
+            }
+        }
+
+        let mut order = vec![];
+        let mut is_unique = true;
+
+        while let Some(v) = que.pop_front() {
+            if que.len() >= 1 {
+                is_unique = false;
+            }
+
+            order.push(v);
+
+            for &nv in &graph[v] {
+                indegrees[nv] -= 1;
+                if indegrees[nv] == 0 {
+                    que.push_back(nv);
+                }
+            }
+        }
+
+        let order = if order.len() == graph.len() {
+            Some(order)
+        } else {
+            is_unique = false;
+            None
+        };
+
+        Self { order, is_unique }
+    }
+
+    pub fn order(&self) -> &Option<Vec<usize>> {
+        &self.order
+    }
+
+    /// トポロジカル順序が一意かどうか
+    /// トポロジカル順序が存在しない場合はfalse
+    pub fn is_unique(&self) -> bool {
+        self.is_unique
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::topological_sort;
+    use super::ToplogicalSort;
 
     #[test]
     fn test_topological_sort() {
+        // TODO: is_uniqueのテスト
+
         let test_cases = vec![
             // test 1
             (
@@ -81,7 +137,8 @@ mod tests {
         ];
 
         for (g, expected) in test_cases {
-            assert_eq!(topological_sort(&g), expected);
+            let t = ToplogicalSort::new(&g);
+            assert_eq!(t.order(), &expected);
         }
     }
 }
