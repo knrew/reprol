7095a5e3a658b0037cfa97701a6df3cf3de5f607
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2025-04-10 01:19:05 +0900

    fix snippet(inversion)

--- snippets/inversion.rs
+++ snippets/inversion.rs
@@ -1,14 +1,14 @@
-/// 転倒数を計算する
+//! 転倒数を計算する
+
 use reprol::{ds::fenwick_tree::FenwickTree, ops::op_add::OpAdd};
+use std::collections::BTreeMap;
 fn inversion<T: Copy + Ord>(v: &[T]) -> u64 {
-    // 座標圧縮
     let (mx, v) = {
         let mut mp = BTreeMap::new();
         for &e in v {
             mp.entry(e).or_insert(0);
         }
         mp.iter_mut().enumerate().for_each(|(i, (_, v))| *v = i);
-
         let mx = mp.len();
         let v = v.iter().map(|e| mp[e]).collect::<Vec<_>>();
         (mx, v)
@@ -18,8 +18,8 @@ fn inversion<T: Copy + Ord>(v: &[T]) -> u64 {
     let mut ft = FenwickTree::<OpAdd<_>>::new(mx);
 
     for vi in v {
-        res += ft.product(vi + 1..);
-        ft.mul(vi, &1);
+        res += ft.fold(vi + 1..);
+        ft.apply(vi, &1);
     }
 
     res
