2e525ee90a51982bff89dcab07263a120d18fb51
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2025-05-05 23:10:20 +0900

    add dijkstra doc

--- src/dijkstra.rs
+++ src/dijkstra.rs
@@ -1,33 +1,139 @@
+//! ダイクストラ(Dijkstra)
+//!
+//! ダイクストラ法で単一始点最短経路を計算する．
+//!
+//! # 使用例
+//! ```
+//! use reprol::dijkstra::Dijkstra;
+//! let start = 0;
+//! let graph = vec![
+//!     vec![(1, 2)],
+//!     vec![(2, 3), (4, 9)],
+//!     vec![(4, 4)],
+//!     vec![(0, 1)],
+//!     vec![],
+//! ];
+//! let dijkstra = Dijkstra::new(
+//!     graph.len(),
+//!     &start,
+//!     &0,
+//!     |&v| v,
+//!     |&v| graph[v].iter().cloned(),
+//! );
+//! assert_eq!(dijkstra.cost(&0), Some(&0));
+//! assert_eq!(dijkstra.cost(&1), Some(&2));
+//! assert_eq!(dijkstra.cost(&2), Some(&5));
+//! assert_eq!(dijkstra.cost(&3), None);
+//! assert_eq!(dijkstra.cost(&4), Some(&9));
+//! ```
+
 use std::{cmp::Reverse, collections::BinaryHeap, fmt::Debug, ops::Add};
 
-/// ダイクストラで最小コストを計算する
-/// V: 頂点の型
-/// C: コストの型
-/// I: 頂点からインデックスへの写像
-pub struct Dijkstra<V, C, I> {
-    /// 頂点数(状態数)
-    n: usize,
+/// 経路情報を管理するためのトレイト．
+pub trait PathTracker<V> {
+    fn new(n: usize) -> Self;
 
-    /// 始点頂点
-    start: V,
+    /// `index`の直前の頂点を返す．
+    fn get_previous(&self, index: usize) -> Option<&V>;
 
-    /// 状態からインデックスへの写像
-    /// 例: H*Wの2次元グリッドのマス(i, j) -> i*w+j
-    to_index: I,
+    /// `index`の直前の頂点を`v`に更新する．
+    fn set_previous(&mut self, index: usize, v: &V);
 
-    /// costs[v]: 頂点startからvへの最短経路のコスト
-    /// startから到達不可能である場合はNone
-    costs: Vec<Option<C>>,
-
-    /// previous_vertices[v]: 最短経路においてvの直前に訪問する頂点
-    previous_vertices: Vec<Option<V>>,
+    /// 始点から`end`までの経路を構築する．
+    fn construct_path<C>(
+        &self,
+        to_index: &impl Fn(&V) -> usize,
+        costs: &[Option<C>],
+        end: &V,
+    ) -> Option<Vec<V>>;
 }
 
-impl<V, C, I> Dijkstra<V, C, I>
+/// 経路を保存する場合に用いる構造体．
+/// 各頂点の直前の頂点を保存する．
+pub struct WithPath<V> {
+    previous: Vec<Option<V>>,
+}
+
+impl<V: Clone> PathTracker<V> for WithPath<V> {
+    fn new(n: usize) -> Self {
+        Self {
+            previous: vec![None; n],
+        }
+    }
+
+    fn get_previous(&self, index: usize) -> Option<&V> {
+        self.previous[index].as_ref()
+    }
+
+    fn set_previous(&mut self, index: usize, value: &V) {
+        self.previous[index] = Some(value.clone());
+    }
+
+    fn construct_path<C>(
+        &self,
+        to_index: &impl Fn(&V) -> usize,
+        costs: &[Option<C>],
+        end: &V,
+    ) -> Option<Vec<V>> {
+        if costs[to_index(end)].is_none() {
+            return None;
+        }
+
+        let mut v = end;
+        let mut path = vec![v];
+
+        while let Some(pv) = self.previous[to_index(v)].as_ref() {
+            path.push(pv);
+            v = pv;
+        }
+
+        Some(path.into_iter().rev().cloned().collect())
+    }
+}
+
+/// 経路を保存しない場合に用いる構造体(ダミー)．
+pub struct NoPath;
+
+impl<V> PathTracker<V> for NoPath {
+    fn new(_: usize) -> Self {
+        Self
+    }
+
+    fn get_previous(&self, _: usize) -> Option<&V> {
+        None
+    }
+
+    fn set_previous(&mut self, _: usize, _: &V) {}
+
+    fn construct_path<C>(
+        &self,
+        _: &impl Fn(&V) -> usize,
+        _: &[Option<C>],
+        _: &V,
+    ) -> Option<Vec<V>> {
+        None
+    }
+}
+
+/// ダイクストラの本体．
+///
+/// - `V`: 頂点の型
+/// - `C`: コストの型
+/// - `I`: `V`をインデックス(`usize`)に変換する関数
+/// - `P`: 経路情報に関する構造体(`WithPath` または `NoPath`)
+pub struct DijkstraImpl<V, C, I, P> {
+    start: V,
+    to_index: I,
+    costs: Vec<Option<C>>,
+    path_tracker: P,
+}
+
+impl<V, C, I, P> DijkstraImpl<V, C, I, P>
 where
     V: Clone + Ord,
     C: Clone + Ord + Add<Output = C>,
     I: Fn(&V) -> usize,
+    P: PathTracker<V>,
 {
     pub fn new<E>(
         n: usize,
@@ -40,7 +146,7 @@ where
         E: Iterator<Item = (V, C)>,
     {
         let mut costs = vec![None; n];
-        let mut previous_vertices = vec![None; n];
+        let mut path_tracker = P::new(n);
 
         let mut heap = BinaryHeap::new();
 
@@ -64,7 +170,7 @@ where
                     Some(cost_nv) if cost_nv <= &new_cost_nv => {}
                     _ => {
                         costs[index_nv] = Some(new_cost_nv.clone());
-                        previous_vertices[index_nv] = Some(v.clone());
+                        path_tracker.set_previous(index_nv, &v);
                         heap.push((Reverse(new_cost_nv), nv));
                     }
                 }
@@ -72,55 +178,44 @@ where
         }
 
         Self {
-            n,
             start: start.clone(),
             to_index,
             costs,
-            previous_vertices,
+            path_tracker,
         }
     }
 
-    pub fn size(&self) -> usize {
-        self.n
+    /// `v`の直前の頂点を返す．
+    ///
+    /// 以下の条件を満たす場合，その時に限り，Noneを返す．
+    /// - `end`に到達できない場合
+    /// - 経路を保存していない場合(`P`が`NoPath`である場合)
+    pub fn previous(&self, v: &V) -> Option<&V> {
+        self.path_tracker.get_previous((self.to_index)(v))
     }
 
+    /// 始点から`end`までの経路を構築する．
+    ///
+    /// 以下の条件を満たす場合，その時に限り，Noneを返す．
+    /// - `end`に到達できない場合
+    /// - 経路を保存していない場合(`P`が`NoPath`である場合)
+    pub fn path(&self, end: &V) -> Option<Vec<V>> {
+        self.path_tracker
+            .construct_path(&self.to_index, &self.costs, end)
+    }
+
+    /// 始点．
     pub fn start(&self) -> &V {
         &self.start
     }
 
-    /// start->vの最小コスト
-    /// startからvへ到達不可能であればNone
+    /// 始点から`v`へのコストを返す．
     pub fn cost(&self, v: &V) -> Option<&C> {
         self.costs[(self.to_index)(v)].as_ref()
     }
-
-    /// startからの最短経路においてvの直前に訪れる頂点
-    /// そのような頂点が存在しなければNone
-    pub fn previous(&self, v: &V) -> Option<&V> {
-        self.previous_vertices[(self.to_index)(v)].as_ref()
-    }
-
-    /// 頂点endへの最短経路を構築する
-    /// startとendを含む
-    pub fn path(&self, end: &V) -> Option<Vec<V>>
-    where
-        V: Clone,
-    {
-        if self.costs[(self.to_index)(end)].is_none() {
-            return None;
-        }
-
-        let mut res = vec![end];
-
-        while let Some(pv) = self.previous(res.last().unwrap()) {
-            res.push(pv);
-        }
-
-        Some(res.into_iter().rev().cloned().collect())
-    }
 }
 
-impl<V, C, I> Debug for Dijkstra<V, C, I>
+impl<V, C, I, P> Debug for DijkstraImpl<V, C, I, P>
 where
     V: Debug,
     C: Debug,
@@ -133,23 +228,14 @@ where
     }
 }
 
-/// 隣接リスト表現のグラフからダイクストラによって最短経路を計算する
-pub fn dijkstra_adjacencies<T>(
-    g: &[Vec<(usize, T)>],
-    start: usize,
-    zero: T,
-) -> Dijkstra<usize, T, impl Fn(&usize) -> usize>
-where
-    T: Clone + Ord + Add<Output = T>,
-{
-    Dijkstra::new(g.len(), &start, &zero, |&v| v, |&v| g[v].iter().cloned())
-}
+pub type Dijkstra<V, C, I> = DijkstraImpl<V, C, I, NoPath>;
+pub type DijkstraWithPath<V, C, I> = DijkstraImpl<V, C, I, WithPath<V>>;
 
 #[cfg(test)]
 mod tests {
     use std::{fmt::Debug, ops::Add};
 
-    use super::dijkstra_adjacencies;
+    use super::*;
 
     struct CostTestCase<'a, T> {
         graph: Vec<Vec<(usize, T)>>,
@@ -163,7 +249,13 @@ mod tests {
     {
         fn test(&self, zero: T) {
             assert_eq!(self.graph.len(), self.expected_costs.len());
-            let dijkstra = dijkstra_adjacencies(&self.graph, self.start, zero);
+            let dijkstra = Dijkstra::new(
+                self.graph.len(),
+                &self.start,
+                &zero,
+                |&v| v,
+                |&v| self.graph[v].iter().cloned(),
+            );
             for v in 0..self.graph.len() {
                 assert_eq!(dijkstra.cost(&v), self.expected_costs[v]);
             }
@@ -171,7 +263,7 @@ mod tests {
     }
 
     #[test]
-    fn test_dijkstra_cost() {
+    fn test_no_path() {
         // case1:
         CostTestCase {
             graph: vec![
@@ -279,28 +371,44 @@ mod tests {
     }
 
     #[test]
-    fn test_dijkstra_path() {
-        let graph = vec![
-            vec![(1, 2)],
-            vec![(2, 3), (4, 9)],
-            vec![(4, 4)],
-            vec![(0, 1)],
-            vec![],
-        ];
-        let start = 0;
-        let dijkstra = dijkstra_adjacencies(&graph, start, 0i32);
-        assert_eq!(dijkstra.path(&4), Some(vec![0, 1, 2, 4]));
-        assert_eq!(dijkstra.path(&3), None);
+    fn test_path() {
+        {
+            let graph = vec![
+                vec![(1, 2)],
+                vec![(2, 3), (4, 9)],
+                vec![(4, 4)],
+                vec![(0, 1)],
+                vec![],
+            ];
+            let start = 0;
+            let dijkstra = DijkstraWithPath::new(
+                graph.len(),
+                &start,
+                &0i32,
+                |&v| v,
+                |&v| graph[v].iter().cloned(),
+            );
+            assert_eq!(dijkstra.path(&4), Some(vec![0, 1, 2, 4]));
+            assert_eq!(dijkstra.path(&3), None);
+        }
 
-        let graph = vec![
-            vec![(1, 2)],
-            vec![(2, 3), (4, 9)],
-            vec![(4, 4)],
-            vec![(0, 1)],
-            vec![],
-        ];
-        let start = 3;
-        let dijkstra = dijkstra_adjacencies(&graph, start, 0i32);
-        assert_eq!(dijkstra.path(&4), Some(vec![3, 0, 1, 2, 4]));
+        {
+            let graph = vec![
+                vec![(1, 2)],
+                vec![(2, 3), (4, 9)],
+                vec![(4, 4)],
+                vec![(0, 1)],
+                vec![],
+            ];
+            let start = 3;
+            let dijkstra = DijkstraWithPath::new(
+                graph.len(),
+                &start,
+                &0i32,
+                |&v| v,
+                |&v| graph[v].iter().cloned(),
+            );
+            assert_eq!(dijkstra.path(&4), Some(vec![3, 0, 1, 2, 4]));
+        }
     }
 }
