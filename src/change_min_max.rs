1bb6a87281fd35ce04088c106376f1a681971542
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2025-06-06 05:29:19 +0900

    rename chminmax

--- src/change_min_max.rs
+++ src/change_min_max.rs
@@ -3,7 +3,7 @@
 //! 最小値・最大値の更新に関するトレイト．
 //!
 //! - [`ChangeMinMax`]：最小値・最大値更新．
-//! - [`ChangeMinMaxOrSet`]：`Option<T>`に対して，代入or最小値・最大値更新．
+//! - [`ChangeMinMaxOrInsert`]：`Option<T>`に対して，代入or最小値・最大値更新．
 //!
 //! # 使用例
 //! ```
@@ -16,13 +16,13 @@
 //!```
 //!
 //!```
-//! use reprol::change_min_max::ChangeMinMaxOrSet;
+//! use reprol::change_min_max::ChangeMinMaxOrInsert;
 //! let mut a: Option<i32> = None;
-//! assert!(a.change_min_or_set(10));
-//! assert!(!a.change_min_or_set(15));
-//! assert!(a.change_min_or_set(5));
-//! assert!(a.change_max_or_set(20));
-//! assert!(!a.change_max_or_set(18));
+//! assert!(a.change_min_or_insert(10));
+//! assert!(!a.change_min_or_insert(15));
+//! assert!(a.change_min_or_insert(5));
+//! assert!(a.change_max_or_insert(20));
+//! assert!(!a.change_max_or_insert(18));
 //! ```
 
 /// 最小値・最大値更新を行うためのトレイト．
@@ -40,6 +40,7 @@ impl<T> ChangeMinMax for T
 where
     T: PartialOrd,
 {
+    #[inline]
     fn change_min(&mut self, rhs: T) -> bool {
         if *self > rhs {
             *self = rhs;
@@ -49,6 +50,7 @@ where
         }
     }
 
+    #[inline]
     fn change_max(&mut self, rhs: T) -> bool {
         if *self < rhs {
             *self = rhs;
@@ -60,25 +62,26 @@ where
 }
 
 /// `Option<T>`に対して，代入or最小値・最大値更新を行うためのトレイト．
-pub trait ChangeMinMaxOrSet {
+pub trait ChangeMinMaxOrInsert {
     type Item;
 
     /// `self`が`None`である場合，または，`self`が`Some(lhs)`で`rhs`の値が`lhs`より小さい場合，`self`を`Some(rhs)`に変更し，trueを返す．
     /// そうでない場合は，何も変更せずにfalseを返す．
-    fn change_min_or_set(&mut self, rhs: Self::Item) -> bool;
+    fn change_min_or_insert(&mut self, rhs: Self::Item) -> bool;
 
     /// `self`が`None`である場合，または，`self`が`Some(lhs)`で`rhs`の値が`lhs`より大きい場合，`self`を`Some(rhs)`に変更し，trueを返す．
     /// そうでない場合は，何も変更せずにfalseを返す．
-    fn change_max_or_set(&mut self, lhs: Self::Item) -> bool;
+    fn change_max_or_insert(&mut self, lhs: Self::Item) -> bool;
 }
 
-impl<T> ChangeMinMaxOrSet for Option<T>
+impl<T> ChangeMinMaxOrInsert for Option<T>
 where
     T: PartialOrd,
 {
     type Item = T;
 
-    fn change_min_or_set(&mut self, rhs: Self::Item) -> bool {
+    #[inline]
+    fn change_min_or_insert(&mut self, rhs: Self::Item) -> bool {
         match self {
             Some(lhs) if *lhs <= rhs => false,
             _ => {
@@ -88,7 +91,8 @@ where
         }
     }
 
-    fn change_max_or_set(&mut self, rhs: Self::Item) -> bool {
+    #[inline]
+    fn change_max_or_insert(&mut self, rhs: Self::Item) -> bool {
         match self {
             Some(lhs) if *lhs >= rhs => false,
             _ => {
@@ -132,29 +136,29 @@ mod tests {
     }
 
     #[test]
-    fn test_change_min_or_set() {
+    fn test_change_min_or_insert() {
         let mut x = None;
-        let f = x.change_min_or_set(10);
+        let f = x.change_min_or_insert(10);
         assert!(f);
         assert_eq!(x, Some(10));
-        let f = x.change_min_or_set(3);
+        let f = x.change_min_or_insert(3);
         assert!(f);
         assert_eq!(x, Some(3));
-        let f = x.change_min_or_set(20);
+        let f = x.change_min_or_insert(20);
         assert!(!f);
         assert_eq!(x, Some(3));
     }
 
     #[test]
-    fn test_change_max_or_set() {
+    fn test_change_max_or_insert() {
         let mut x = None;
-        let f = x.change_max_or_set(10);
+        let f = x.change_max_or_insert(10);
         assert!(f);
         assert_eq!(x, Some(10));
-        let f = x.change_max_or_set(20);
+        let f = x.change_max_or_insert(20);
         assert!(f);
         assert_eq!(x, Some(20));
-        let f = x.change_max_or_set(2);
+        let f = x.change_max_or_insert(2);
         assert!(!f);
         assert_eq!(x, Some(20));
     }
