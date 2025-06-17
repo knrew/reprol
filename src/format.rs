99af6929fc40d596f69d591f3661f26ffce94e74
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2025-06-17 17:08:03 +0900

    update vec formatter

--- src/format.rs
+++ src/format.rs
@@ -1,88 +1,157 @@
 //! 出力時に便利な関数など
 
-use std::fmt::{Display, Write};
+use std::fmt::{Debug, Display};
 
-pub trait IterFormatter {
-    /// 要素のイテレータからsep区切りで文字列に変換する．
-    fn to_string(&mut self, sep: &str) -> String;
-}
-
-impl<I> IterFormatter for I
+fn format_iter_display<I>(
+    f: &mut std::fmt::Formatter<'_>,
+    iter: &mut I,
+    sep: &str,
+) -> std::fmt::Result
 where
     I: Iterator,
     I::Item: Display,
 {
-    fn to_string(&mut self, sep: &str) -> String {
-        let mut res = String::new();
-        if let Some(item) = self.next() {
-            write!(&mut res, "{}", item).unwrap();
-            while let Some(item) = self.next() {
-                write!(&mut res, "{}{}", sep, item).unwrap();
+    if let Some(item) = iter.next() {
+        write!(f, "{}", item)?;
+        while let Some(item) = iter.next() {
+            write!(f, "{}{}", sep, item)?;
+        }
+    }
+    Ok(())
+}
+
+fn format_iter_debug<I>(
+    f: &mut std::fmt::Formatter<'_>,
+    iter: &mut I,
+    sep: &str,
+) -> std::fmt::Result
+where
+    I: Iterator,
+    I::Item: Debug,
+{
+    if let Some(item) = iter.next() {
+        write!(f, "{:?}", item)?;
+        while let Some(item) = iter.next() {
+            write!(f, "{}{:?}", sep, item)?;
+        }
+    }
+    Ok(())
+}
+
+pub struct FormatIter<I>(pub I, pub &'static str);
+pub struct FormatVec<'a, T>(pub &'a [T], pub &'static str);
+pub struct FormatUsize1Vec<'a>(pub &'a [usize], pub &'static str);
+pub struct FormatChars<'a>(pub &'a [char]);
+pub struct FormatBytes<'a>(pub &'a [u8]);
+
+macro_rules! impl_fmt {
+    ($fmt_trait:ident, $fmt_func:ident) => {
+        impl<I> $fmt_trait for FormatIter<I>
+        where
+            I: Iterator + Clone,
+            I::Item: $fmt_trait,
+        {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let mut iter = self.0.clone();
+                let sep = self.1;
+                $fmt_func(f, &mut iter, sep)
             }
         }
-        res
-    }
+
+        impl<'a, T> $fmt_trait for FormatVec<'a, T>
+        where
+            T: $fmt_trait,
+        {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let mut iter = self.0.iter();
+                let sep = self.1;
+                $fmt_func(f, &mut iter, sep)
+            }
+        }
+
+        impl<'a> $fmt_trait for FormatUsize1Vec<'a> {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let mut iter = self.0.iter().map(|i| i + 1);
+                let sep = self.1;
+                $fmt_func(f, &mut iter, sep)
+            }
+        }
+
+        impl<'a> $fmt_trait for FormatChars<'a> {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let mut iter = self.0.iter();
+                $fmt_func(f, &mut iter, "")
+            }
+        }
+
+        impl<'a> $fmt_trait for FormatBytes<'a> {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let mut iter = self.0.iter().map(|&c| c as char);
+                $fmt_func(f, &mut iter, "")
+            }
+        }
+    };
 }
 
-pub trait ArrayFormatter {
-    /// 配列をsep区切りで文字列に変換する．
-    fn to_string(&self, sep: &str) -> String;
-}
-
-impl<T> ArrayFormatter for [T]
-where
-    T: Display,
-{
-    fn to_string(&self, sep: &str) -> String {
-        self.iter().to_string(sep)
-    }
-}
-
-pub trait Usize1ArrayFormatter {
-    /// usizeの配列を1-indexedの文字列に変換する．
-    fn to_string_usize1(&self, sep: &str) -> String;
-}
-
-impl Usize1ArrayFormatter for [usize] {
-    fn to_string_usize1(&self, sep: &str) -> String {
-        self.iter().map(|i| i + 1).to_string(sep)
-    }
-}
-
-pub trait CharsFormatter {
-    // char型またはu8型の配列を文字列に変換する．
-    fn as_string(&self) -> String;
-}
-
-impl CharsFormatter for [char] {
-    fn as_string(&self) -> String {
-        self.iter().collect::<String>()
-    }
-}
-
-impl CharsFormatter for [u8] {
-    fn as_string(&self) -> String {
-        self.iter().map(|&c| c as char).collect::<String>()
-    }
-}
+impl_fmt!(Display, format_iter_display);
+impl_fmt!(Debug, format_iter_debug);
 
 #[cfg(test)]
 mod tests {
-    use super::{ArrayFormatter, CharsFormatter, IterFormatter};
+    use super::*;
 
     #[test]
-    fn test_as_string() {
-        let v = vec![1, 2, 3, 4, 5];
-        assert_eq!(v.iter().to_string(" "), "1 2 3 4 5");
-        assert_eq!(v.to_string(" "), "1 2 3 4 5");
+    fn test_format_iter() {
+        {
+            let v = vec![3, 1, 4, 1, 5];
+            assert_eq!(format!("{}", FormatIter(v.iter(), " ")), "3 1 4 1 5");
+            assert_eq!(format!("{}", FormatIter(v.iter(), ", ")), "3, 1, 4, 1, 5");
+
+            assert_eq!(format!("{:?}", FormatIter(v.iter(), " ")), "3 1 4 1 5");
+            assert_eq!(format!("{:?}", FormatIter(v.iter(), ", ")), "3, 1, 4, 1, 5");
+        }
+
+        {
+            use std::collections::VecDeque;
+            let v = VecDeque::from(vec![2, 7, 1, 8, 2]);
+            assert_eq!(format!("{}", FormatIter(v.iter(), " ")), "2 7 1 8 2");
+            assert_eq!(format!("{}", FormatIter(v.iter(), ", ")), "2, 7, 1, 8, 2");
+
+            assert_eq!(format!("{:?}", FormatIter(v.iter(), " ")), "2 7 1 8 2");
+            assert_eq!(format!("{:?}", FormatIter(v.iter(), ", ")), "2, 7, 1, 8, 2");
+        }
     }
 
     #[test]
-    fn test_to_string() {
-        assert_eq!(vec!['A', 'B', 'C'].as_string(), String::from("ABC"));
-        assert_eq!(['d', 'e', 'f'].as_string(), String::from("def"));
-        assert_eq!(vec!['1', '2', '3'].as_string(), String::from("123"));
-        assert_eq!(vec![b'A', b'B', b'C'].as_string(), String::from("ABC"));
-        assert_eq!(b"def".as_string(), String::from("def"));
+    fn test_format_vec() {
+        let v = vec![3, 1, 4, 1, 5];
+        assert_eq!(format!("{}", FormatVec(&v, " ")), "3 1 4 1 5");
+        assert_eq!(format!("{}", FormatVec(&v, ", ")), "3, 1, 4, 1, 5");
+
+        assert_eq!(format!("{:?}", FormatVec(&v, " ")), "3 1 4 1 5");
+        assert_eq!(format!("{:?}", FormatVec(&v, ", ")), "3, 1, 4, 1, 5");
+    }
+
+    #[test]
+    fn test_format_usize1_vec() {
+        let v = vec![0, 1, 2, 3, 4];
+        assert_eq!(format!("{}", FormatUsize1Vec(&v, " ")), "1 2 3 4 5");
+        assert_eq!(format!("{}", FormatUsize1Vec(&v, ", ")), "1, 2, 3, 4, 5");
+
+        assert_eq!(format!("{:?}", FormatUsize1Vec(&v, " ")), "1 2 3 4 5");
+        assert_eq!(format!("{:?}", FormatUsize1Vec(&v, ", ")), "1, 2, 3, 4, 5");
+    }
+
+    #[test]
+    fn test_format_chars() {
+        assert_eq!(format!("{}", FormatChars(&vec!['A', 'B', 'C'])), "ABC");
+        assert_eq!(format!("{}", FormatChars(&['d', 'e', 'f'])), "def");
+        assert_eq!(format!("{}", FormatChars(&['1', '2', '3'])), "123");
+    }
+
+    #[test]
+    fn test_format_bytes() {
+        assert_eq!(format!("{}", FormatBytes(&vec![b'A', b'B', b'C'])), "ABC");
+        assert_eq!(format!("{}", FormatBytes(b"def")), "def");
     }
 }
