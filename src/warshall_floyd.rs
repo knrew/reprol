b3dffb657e67c14fe3a166267de29e0dd6da4958
knrew <rew.7cc.12.1f@gmail.com>
2025-07-27 04:59:16 +0900

    add incremental update for warshall floyd

--- src/warshall_floyd.rs
+++ src/warshall_floyd.rs
@@ -1,67 +1,131 @@
+//! ワーシャル・フロイド(Warshall Floyd)
+//!
+//! ワーシャル・フロイド法によって全点間最短経路を計算する．
+//!
+//! # 使用例
+//! ```
+//! use reprol::warshall_floyd::WarshallFloyd;
+//! let mut wf = WarshallFloyd::new(3, 0);
+//! wf.add_edge(0, 1, 4);
+//! wf.add_edge(1, 2, 2);
+//! wf.build();
+//! assert_eq!(wf.cost(0, 2), Some(&6));
+//! wf.add_edge_incremental(0, 2, 1);
+//! assert_eq!(wf.cost(0, 2), Some(&1));
+//! ```
+
 use std::ops::Add;
 
-/// ワーシャルフロイド法によって頂点間の最小コストを計算する
+/// ワーシャル・フロイド本体．
 pub struct WarshallFloyd<C> {
     costs: Vec<Vec<Option<C>>>,
-    has_negative_cycle: bool,
+    zero: C,
+    has_build: bool,
 }
 
 impl<C> WarshallFloyd<C>
 where
     C: Clone + PartialOrd + Add<Output = C>,
 {
-    /// 隣接リスト表現のグラフから最短経路を計算する
-    pub fn new(graph: &[Vec<(usize, C)>], zero: &C) -> Self {
-        let n = graph.len();
+    pub fn new(n: usize, zero: C) -> Self {
         let mut costs = vec![vec![None; n]; n];
 
         for i in 0..n {
             costs[i][i] = Some(zero.clone());
         }
 
-        for i in 0..n {
-            for &(j, ref c) in &graph[i] {
-                if !costs[i][j].as_ref().is_some_and(|cost_ij| cost_ij <= c) {
-                    costs[i][j] = Some(c.clone());
-                }
-            }
+        Self {
+            costs,
+            zero,
+            has_build: true,
         }
+    }
 
+    /// 全点間最短経路を計算する．
+    ///
+    /// - 計算量: O(N^3)
+    pub fn build(&mut self) {
+        self.has_build = true;
+        let n = self.costs.len();
         for k in 0..n {
             for i in 0..n {
                 for j in 0..n {
-                    // i->k, k->jがともに存在すれば，i->jが存在する
-                    if let Some(cost_ik) = &costs[i][k] {
-                        if let Some(cost_kj) = &costs[k][j] {
+                    match (&self.costs[i][k], &self.costs[k][j]) {
+                        (Some(cost_ik), Some(cost_kj)) => {
                             let new_cost = cost_ik.clone() + cost_kj.clone();
-                            if !costs[i][j]
+                            if !self.costs[i][j]
                                 .as_ref()
                                 .is_some_and(|cost_ij| cost_ij <= &new_cost)
                             {
-                                costs[i][j] = Some(new_cost);
+                                self.costs[i][j] = Some(new_cost);
                             }
                         }
+                        _ => {}
                     }
                 }
             }
         }
+    }
 
-        let has_negative_cycle = (0..n).any(|v| costs[v][v].as_ref().is_some_and(|c| c < &zero));
-
-        Self {
-            costs,
-            has_negative_cycle,
+    /// 頂点`u`から`v`へコスト`c`の有向辺を追加する．
+    /// コストの計算は行われない．
+    pub fn add_edge(&mut self, u: usize, v: usize, c: C) {
+        self.has_build = false;
+        if !self.costs[u][v]
+            .as_ref()
+            .is_some_and(|cost_uv| cost_uv <= &c)
+        {
+            self.costs[u][v] = Some(c);
         }
     }
 
-    /// u->vの最小コスト
+    /// 頂点`u`から`v`へコスト`c`の有向辺を追加し，コストの(差分)更新を行う．
+    ///
+    /// - 計算量: O(N^2)
+    pub fn add_edge_incremental(&mut self, u: usize, v: usize, c: C) {
+        assert!(self.has_build);
+
+        let n = self.costs.len();
+
+        if !self.costs[u][v]
+            .as_ref()
+            .is_some_and(|cost_uv| cost_uv <= &c)
+        {
+            self.costs[u][v] = Some(c.clone());
+
+            for s in 0..n {
+                for g in 0..n {
+                    match (&self.costs[s][u], &self.costs[v][g]) {
+                        (Some(cost_su), Some(cost_vg)) => {
+                            let new_cost = cost_su.clone() + c.clone() + cost_vg.clone();
+                            if !self.costs[s][g]
+                                .as_ref()
+                                .is_some_and(|cost_sg| cost_sg <= &new_cost)
+                            {
+                                self.costs[s][g] = Some(new_cost);
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+
+    /// 頂点`u`から`v`への最小コストを返す．
+    /// `build()`を呼んでコストを計算してから呼び出す．
     pub fn cost(&self, u: usize, v: usize) -> Option<&C> {
+        assert!(self.has_build);
         self.costs[u][v].as_ref()
     }
 
-    /// 負の閉路が存在するか
+    /// 負の閉路が存在するかを判定する．
+    ///
+    /// - 計算量：O(N)
     pub fn has_negative_cycle(&self) -> bool {
-        self.has_negative_cycle
+        assert!(self.has_build);
+        let n = self.costs.len();
+        (0..n).any(|v| self.costs[v][v].as_ref().is_some_and(|c| c < &self.zero))
     }
 }
 
@@ -70,79 +134,94 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_warshall_floyd() {
-        // case 1: 連結
-        {
-            let n = 3;
-            // (u, v, c): u->vのコストがc
-            let edges: Vec<(usize, usize, i32)> = vec![(0, 1, 1), (1, 0, 2), (1, 2, 3), (2, 0, 4)];
-            let g = {
-                let mut g = vec![vec![]; n];
-                for &(u, v, c) in &edges {
-                    g[u].push((v, c));
-                }
-                g
-            };
-            let wf = WarshallFloyd::new(&g, &0);
-            assert!(!wf.has_negative_cycle());
-            assert_eq!(wf.cost(0, 0), Some(&0));
-            assert_eq!(wf.cost(0, 1), Some(&1));
-            assert_eq!(wf.cost(0, 2), Some(&4));
-            assert_eq!(wf.cost(1, 0), Some(&2));
-            assert_eq!(wf.cost(1, 1), Some(&0));
-            assert_eq!(wf.cost(1, 2), Some(&3));
-            assert_eq!(wf.cost(2, 0), Some(&4));
-            assert_eq!(wf.cost(2, 1), Some(&5));
-            assert_eq!(wf.cost(2, 2), Some(&0));
-        }
+    fn test_positive_cost() {
+        let n = 3;
+        let edges: Vec<(usize, usize, i32)> = vec![(0, 1, 1), (1, 0, 2), (1, 2, 3), (2, 0, 4)];
 
-        // case 2
-        {
-            let n = 5;
-            let edges: Vec<(usize, usize, i64)> =
-                vec![(0, 1, 4), (1, 2, 5), (0, 2, -10), (3, 4, 8), (4, 3, 12)];
-            let g = {
-                let mut g = vec![vec![]; n];
-                for &(u, v, c) in &edges {
-                    g[u].push((v, c));
-                }
-                g
-            };
-            let wf = WarshallFloyd::new(&g, &0);
-            assert!(!wf.has_negative_cycle());
-            assert_eq!(wf.cost(0, 0), Some(&0));
-            assert_eq!(wf.cost(0, 1), Some(&4));
-            assert_eq!(wf.cost(0, 2), Some(&-10));
-            assert_eq!(wf.cost(0, 3), None);
-            assert_eq!(wf.cost(0, 4), None);
-            assert_eq!(wf.cost(1, 0), None);
-            assert_eq!(wf.cost(1, 1), Some(&0));
-            assert_eq!(wf.cost(1, 2), Some(&5));
-            assert_eq!(wf.cost(3, 2), None);
-            assert_eq!(wf.cost(3, 4), Some(&8));
-            assert_eq!(wf.cost(4, 3), Some(&12));
+        let mut wf = WarshallFloyd::new(n, 0);
+        for &(u, v, c) in &edges {
+            wf.add_edge(u, v, c);
         }
+        wf.build();
 
-        // case 3: 負の閉路あり
-        {
-            let n = 4;
-            let edges: Vec<(usize, usize, i64)> = vec![
-                (0, 1, 1),
-                (0, 2, 5),
-                (1, 2, 2),
-                (1, 3, 4),
-                (2, 3, 1),
-                (3, 2, -7),
-            ];
-            let g = {
-                let mut g = vec![vec![]; n];
-                for &(u, v, c) in &edges {
-                    g[u].push((v, c));
-                }
-                g
-            };
-            let wf = WarshallFloyd::new(&g, &0);
-            assert!(wf.has_negative_cycle());
+        assert!(!wf.has_negative_cycle());
+        assert_eq!(wf.cost(0, 0), Some(&0));
+        assert_eq!(wf.cost(0, 1), Some(&1));
+        assert_eq!(wf.cost(0, 2), Some(&4));
+        assert_eq!(wf.cost(1, 0), Some(&2));
+        assert_eq!(wf.cost(1, 1), Some(&0));
+        assert_eq!(wf.cost(1, 2), Some(&3));
+        assert_eq!(wf.cost(2, 0), Some(&4));
+        assert_eq!(wf.cost(2, 1), Some(&5));
+        assert_eq!(wf.cost(2, 2), Some(&0));
+    }
+
+    #[test]
+    fn test_negative_cost() {
+        let n = 5;
+        let edges: Vec<(usize, usize, i64)> =
+            vec![(0, 1, 4), (1, 2, 5), (0, 2, -10), (3, 4, 8), (4, 3, 12)];
+
+        let mut wf = WarshallFloyd::new(n, 0);
+        for &(u, v, c) in &edges {
+            wf.add_edge(u, v, c);
         }
+        wf.build();
+
+        assert!(!wf.has_negative_cycle());
+        assert_eq!(wf.cost(0, 0), Some(&0));
+        assert_eq!(wf.cost(0, 1), Some(&4));
+        assert_eq!(wf.cost(0, 2), Some(&-10));
+        assert_eq!(wf.cost(0, 3), None);
+        assert_eq!(wf.cost(0, 4), None);
+        assert_eq!(wf.cost(1, 0), None);
+        assert_eq!(wf.cost(1, 1), Some(&0));
+        assert_eq!(wf.cost(1, 2), Some(&5));
+        assert_eq!(wf.cost(3, 2), None);
+        assert_eq!(wf.cost(3, 4), Some(&8));
+        assert_eq!(wf.cost(4, 3), Some(&12));
+    }
+
+    #[test]
+    fn test_negative_cycle() {
+        let n = 4;
+        let edges: Vec<(usize, usize, i64)> = vec![
+            (0, 1, 1),
+            (0, 2, 5),
+            (1, 2, 2),
+            (1, 3, 4),
+            (2, 3, 1),
+            (3, 2, -7),
+        ];
+
+        let mut wf = WarshallFloyd::new(n, 0);
+        for &(u, v, c) in &edges {
+            wf.add_edge(u, v, c);
+        }
+        wf.build();
+
+        assert!(wf.has_negative_cycle());
+    }
+
+    #[test]
+    fn test_add_edge_incremental() {
+        let mut wf = WarshallFloyd::new(3, 0);
+        wf.add_edge(0, 1, 5);
+        wf.add_edge(1, 2, 7);
+        wf.build();
+
+        wf.add_edge_incremental(0, 2, 20);
+        assert_eq!(wf.cost(0, 2), Some(&12));
+
+        wf.add_edge_incremental(0, 2, 8);
+        assert_eq!(wf.cost(0, 2), Some(&8));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_cost_without_build() {
+        let mut wf = WarshallFloyd::new(2, 0);
+        wf.add_edge(0, 1, 1);
+        let _ = wf.cost(0, 1);
     }
 }
