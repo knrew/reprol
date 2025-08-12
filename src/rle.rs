11589caa7ac987474fa2bf57def21051d88f86bb
knrew <rew.7cc.12.1f@gmail.com>
2025-08-13 07:09:50 +0900

    update rle iterator

--- src/rle.rs
+++ src/rle.rs
@@ -9,39 +9,85 @@
 //! use reprol::rle::Rle;
 //!
 //! let s = b"aaabbbbcc";
-//! assert_eq!(s.rle(), vec![(b'a', 3), (b'b', 4), (b'c', 2)]);
+//! assert!(s.rle().eq([(&b'a', 3), (&b'b', 4), (&b'c', 2)]));
 //!
 //! let v = vec![1, 1, 2, 2, 2, 3, 3];
-//! assert_eq!(v.rle(), vec![(1, 2), (2, 3), (3, 2)]);
+//! assert!(v.rle().eq([(&1, 2), (&2, 3), (&3, 2)]));
 //! ```
 
-pub trait Rle {
-    type Output;
-
-    fn rle(&self) -> Self::Output;
+pub struct RleIter<'a, T> {
+    slice: &'a [T],
+    l: usize,
+    r: usize,
 }
 
-impl<T> Rle for [T]
-where
-    T: Clone + PartialEq,
-{
-    type Output = Vec<(T, usize)>;
+impl<'a, T: PartialEq> RleIter<'a, T> {
+    fn new(s: &'a [T]) -> Self {
+        Self {
+            slice: s,
+            l: 0,
+            r: s.len(),
+        }
+    }
+}
 
-    fn rle(&self) -> Self::Output {
-        let mut res = vec![];
+impl<'a, T: PartialEq> Iterator for RleIter<'a, T> {
+    type Item = (&'a T, usize);
 
-        for e in self {
-            match res.last_mut() {
-                Some((last_e, count)) if last_e == e => {
-                    *count += 1;
-                }
-                _ => {
-                    res.push((e.clone(), 1));
-                }
-            }
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.l >= self.r {
+            return None;
         }
 
-        res
+        let start = self.l;
+        self.l += 1;
+        while self.l < self.r && self.slice[self.l] == self.slice[start] {
+            self.l += 1;
+        }
+
+        Some((&self.slice[start], self.l - start))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.r - self.l))
+    }
+}
+
+impl<'a, T: PartialEq> DoubleEndedIterator for RleIter<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.l >= self.r {
+            return None;
+        }
+
+        let mut j = self.r - 1;
+        let value = &self.slice[j];
+
+        while j > self.l && self.slice[j - 1] == *value {
+            j -= 1;
+        }
+
+        let count = self.r - j;
+        self.r = j;
+
+        Some((value, count))
+    }
+}
+
+pub trait Rle {
+    type Item;
+    type Iter<'a>: Iterator<Item = (&'a Self::Item, usize)>
+    where
+        Self: 'a;
+
+    fn rle(&self) -> Self::Iter<'_>;
+}
+
+impl<T: PartialEq> Rle for [T] {
+    type Item = T;
+    type Iter<'a> = RleIter<'a, T> where T:'a;
+
+    fn rle(&self) -> Self::Iter<'_> {
+        RleIter::new(self)
     }
 }
 
@@ -52,13 +98,13 @@ mod tests {
     #[test]
     fn test_rle() {
         let s = "aaabbbcccddeee".chars().collect::<Vec<char>>();
-        let expected = vec![('a', 3), ('b', 3), ('c', 3), ('d', 2), ('e', 3)];
-        assert_eq!(s.rle(), expected);
+        let expected = vec![(&'a', 3), (&'b', 3), (&'c', 3), (&'d', 2), (&'e', 3)];
+        assert!(s.rle().eq(expected));
 
         let s = "abcde".chars().collect::<Vec<char>>();
-        let expected = vec![('a', 1), ('b', 1), ('c', 1), ('d', 1), ('e', 1)];
-        assert_eq!(s.rle(), expected);
+        let expected = vec![(&'a', 1), (&'b', 1), (&'c', 1), (&'d', 1), (&'e', 1)];
+        assert!(s.rle().eq(expected));
 
-        assert_eq!(Vec::<char>::new().rle(), Vec::new());
+        assert!(Vec::<char>::new().rle().eq(vec![]));
     }
 }
