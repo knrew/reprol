303526cbdb1e7f5b4f0dceda70363534cd4a4c04
knrew <rew.7cc.12.1f@gmail.com>
2025-08-13 03:42:03 +0900

    refactor div_ceil/div_floor

--- src/math/div_ceil.rs
+++ src/math/div_ceil.rs
@@ -1,151 +1,160 @@
+//! div_ceil
+//!
+//! 2個の整数x, yに対して，x/y以上の最小の整数を計算する．
+//! 負の数対応．
+//! $\lceil \frac{x}{y} \rceil$
+//!
+//!
+//! ## 使用例
+//! ```
+//! use reprol::math::div_ceil::DivCeil;
+//! assert_eq!(5i32.div_ceil_(3), 2);
+//! assert_eq!((-4i32).div_ceil_(3), -1);
+//! ```
+
 pub trait DivCeil {
-    /// $\lceil \frac{x}{y} \rceil$を計算する 小数点以下切り上げ
-    /// NOTE: stdのdiv_ceilはrustc1.73から
     fn div_ceil_(self, rhs: Self) -> Self;
 }
 
-macro_rules! impl_signed {
-    ($($ty:ident),*) => {$(
+macro_rules! impl_div_signed {
+    ($ty: ty) => {
         impl DivCeil for $ty {
             fn div_ceil_(self, rhs: Self) -> Self {
                 assert!(rhs != 0);
-                if (self >= 0) == (rhs > 0) {
-                    let lhs = self.abs();
-                    let rhs = rhs.abs();
-                    let d = lhs / rhs;
-                    let r = lhs % rhs;
-                    if r > 0 {
-                        d + 1
-                    } else {
-                        d
-                    }
-                } else {
-                    self / rhs
-                }
-            }
-        }
-    )*};
-}
-
-impl_signed! { i8, i16, i32, i64, i128, isize }
-
-macro_rules! impl_unsigned {
-    ($($ty:ident),*) => {$(
-        impl DivCeil for $ty {
-            fn div_ceil_(self, rhs: Self) -> Self {
-                assert!(rhs != 0);
-                let d = self / rhs;
+                let q = self / rhs;
                 let r = self % rhs;
-                if r > 0 {
-                    d + 1
+                if r != 0 && ((self >= 0) == (rhs >= 0)) {
+                    q + 1
                 } else {
-                    d
+                    q
                 }
             }
         }
-    )*};
+    };
 }
 
-impl_unsigned! { u8, u16, u32, u64, u128, usize }
+macro_rules! impl_div_unsigned {
+    ($ty: ty) => {
+        impl DivCeil for $ty {
+            fn div_ceil_(self, rhs: Self) -> Self {
+                assert!(rhs != 0);
+                let q = self / rhs;
+                let r = self % rhs;
+                if r != 0 {
+                    q + 1
+                } else {
+                    q
+                }
+            }
+        }
+    };
+}
+
+macro_rules! impl_div_for {
+    (unsigned: [$($u:ty),* $(,)?], signed: [$($s:ty),* $(,)?]$(,)?) => {
+        $( impl_div_unsigned!($u); )*
+        $( impl_div_signed!($s); )*
+    };
+}
+
+impl_div_for! {
+    unsigned: [u8, u16, u32, u64, u128, usize],
+    signed:   [i8, i16, i32, i64, i128, isize],
+}
 
 #[cfg(test)]
 mod tests {
-    use super::DivCeil;
+    use super::*;
 
     #[test]
-    fn test_div_ceil_i32() {
-        // (lhs, rhs, ans)
-        let test_cases: Vec<(i32, i32, i32)> = vec![
-            (0, 6, 0),
-            (196816367, 408936581, 1),
-            (1469330793, 211923681, 7),
-            (524989232, 1521503399, 1),
-            (1980481447, 743305495, 3),
-            (994578483, 91868066, 11),
-            (164452122, 233341757, 1),
-            (892769521, 750125500, 2),
-            (535341248, 1863750977, 1),
-            (436725017, 10409309, 42),
-            (2137006360, 1895544918, 2),
+    fn test_i32() {
+        let testcases: &[(i32, i32, i32)] = &[
+            //
+            // 正の数どうし
+            //
+            (0, 1, 0),
+            (1, 1, 1),
+            (1, 2, 1),
+            (2, 1, 2),
+            (2, 2, 1),
+            (3, 2, 2),
+            (5, 2, 3),
+            (6, 3, 2),
+            (7, 3, 3),
+            (10, 4, 3),
+            (100, 9, 12),
             (i32::MAX, i32::MAX, 1),
-            (i32::MAX, 1, i32::MAX),
+            (i32::MAX - 1, i32::MAX, 1),
+            //
+            //負の数あり
+            //
+            (-4, 3, -1),
+            (4, -3, -1),
+            (-4, -3, 2),
             (-7, 3, -2),
-            (-9, 3, -3),
             (7, -3, -2),
-            (9, -3, -3),
             (-7, -3, 3),
-            (-9, -3, 3),
-            (0, 5, 0),
+            (i32::MIN, 1, i32::MIN),
+            (i32::MAX, -1, -i32::MAX),
             (0, -5, 0),
         ];
-        for (lhs, rhs, ans) in test_cases {
-            assert_eq!(lhs.div_ceil_(rhs), ans);
+
+        for &(a, b, expected) in testcases {
+            assert_eq!(a.div_ceil_(b), expected, "failed case: {a} div_ceil {b}");
         }
     }
 
     #[test]
-    fn test_div_ceil_u32() {
-        // (lhs, rhs, ans)
-        let test_cases: Vec<(u32, u32, u32)> = vec![
-            (3242167662, 4067889619, 1),
-            (3228637172, 2099291377, 2),
-            (1627038402, 2683980054, 1),
-            (3754350367, 1415834133, 3),
-            (3429753677, 720984332, 5),
-            (2829652990, 539026088, 6),
-            (4003110728, 1367184956, 3),
-            (2809049968, 2899476733, 1),
-            (1752283686, 963336685, 2),
-            (2791639793, 496148063, 6),
-            (u32::MAX, u32::MAX, 1),
-            (u32::MAX, 1, u32::MAX),
+    fn test_u64() {
+        let cases: &[(u64, u64)] = &[
+            (0, 1),
+            (1, 1),
+            (1, 2),
+            (2, 1),
+            (3, 2),
+            (u64::MAX, u64::MAX),
+            (u64::MAX - 1, u64::MAX),
+            (u64::MAX, 2),
+            (u64::MAX - 5, 3),
         ];
-        for (lhs, rhs, ans) in test_cases {
-            assert_eq!(lhs.div_ceil_(rhs), ans);
+
+        for &(a, b) in cases {
+            let expected = if a == 0 { 0 } else { 1 + (a - 1) / b };
+            assert_eq!(a.div_ceil_(b), expected, "failed case: {a} div_ceil {b}");
         }
     }
 
     #[test]
-    fn test_div_ceil_i64() {
-        // (lhs, rhs, ans)
-        let test_cases: Vec<(i64, i64, i64)> = vec![
-            (3743961514775493914, 7847710520604452858, 1),
-            (3658785061199668031, 4941439423322782315, 1),
-            (189632417364924173, 8997957903449639156, 1),
-            (2791055268025099259, 2619683137426655400, 2),
-            (1734403341746549421, 6923822879703219680, 1),
-            (5077729266857648076, 5157158773342006445, 1),
-            (4994375334885718818, 1657802092686856189, 4),
-            (2750481411247349115, 8920783884410743561, 1),
-            (6843860248342457318, 1641229231135444988, 5),
-            (5622981829400302400, 4534692062369137460, 2),
-            (i64::MAX, i64::MAX, 1),
-            (i64::MAX, 1, i64::MAX),
-        ];
-        for (lhs, rhs, ans) in test_cases {
-            assert_eq!(lhs.div_ceil_(rhs), ans);
-        }
+    fn test_smoke_all_types() {
+        assert_eq!(5u8.div_ceil_(2), 3);
+        assert_eq!(5u16.div_ceil_(2), 3);
+        assert_eq!(5u32.div_ceil_(2), 3);
+        assert_eq!(5u64.div_ceil_(2), 3);
+        assert_eq!(5u128.div_ceil_(2), 3);
+        assert_eq!(5usize.div_ceil_(2), 3);
+        assert_eq!(5i8.div_ceil_(2), 3);
+        assert_eq!(5i16.div_ceil_(2), 3);
+        assert_eq!(5i32.div_ceil_(2), 3);
+        assert_eq!(5i64.div_ceil_(2), 3);
+        assert_eq!(5i128.div_ceil_(2), 3);
+        assert_eq!(5isize.div_ceil_(2), 3);
     }
 
     #[test]
-    fn test_div_ceil_u64() {
-        // (lhs, rhs, ans)
-        let test_cases: Vec<(u64, u64, u64)> = vec![
-            (2430709731614043528, 3632250489442770625, 1),
-            (570109040799770453, 6702605256062026021, 1),
-            (965384831889102938, 8268487403814979940, 1),
-            (9206659733475386584, 3804307026053483427, 3),
-            (17152143308950541775, 15576234526326580843, 2),
-            (15332043456037348367, 9273459586063261272, 2),
-            (1420003914580467124, 3059658280927341758, 1),
-            (11700379081278641973, 12753136605423676479, 1),
-            (13665743024848294134, 9100183548711498319, 2),
-            (8436937732911287877, 11407520177484861707, 1),
-            (u64::MAX, u64::MAX, 1),
-            (u64::MAX, 1, u64::MAX),
-        ];
-        for (lhs, rhs, ans) in test_cases {
-            assert_eq!(lhs.div_ceil_(rhs), ans);
-        }
+    #[should_panic]
+    fn test_zero_div_i32() {
+        let _ = 1i32.div_ceil_(0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_zero_div_u64() {
+        let _ = 1u64.div_ceil_(0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_i32_min_overflow_panics() {
+        let _ = i32::MIN.div_ceil_(-1);
     }
 }
--- src/math/div_floor.rs
+++ src/math/div_floor.rs
@@ -1,72 +1,158 @@
+//! div_floor
+//!
+//! 2個の整数x, yに対して，x/y以下の最大の整数を計算する．
+//! 負の数対応．
+//! $\lfloor \frac{x}{y} \rfloor$
+//!
+//! ## 使用例
+//! ```
+//! use reprol::math::div_floor::DivFloor;
+//! assert_eq!(7i32.div_floor_(3), 2);
+//! assert_eq!((-4i32).div_floor_(3), -2);
+//! ```
+
 pub trait DivFloor {
-    /// $\lfloor \frac{x}{y} \rfloor$を計算する
     fn div_floor_(self, rhs: Self) -> Self;
 }
 
-macro_rules! impl_integer {
-    ($($ty:ident),*) => {$(
+macro_rules! impl_div_unsigned {
+    ($ty: ty) => {
         impl DivFloor for $ty {
-            #[allow(unused_comparisons)]
+            fn div_floor_(self, rhs: Self) -> Self {
+                assert!(rhs != 0);
+                self / rhs
+            }
+        }
+    };
+}
+
+macro_rules! impl_div_signed {
+    ($ty: ty) => {
+        impl DivFloor for $ty {
             fn div_floor_(self, rhs: Self) -> Self {
                 assert!(rhs != 0);
                 let q = self / rhs;
                 let r = self % rhs;
                 if r != 0 && (self < 0) != (rhs < 0) {
-                        q - 1
-                    } else {
-                        q
+                    q - 1
+                } else {
+                    q
                 }
             }
         }
-    )*};
+    };
 }
 
-impl_integer! { i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize }
+macro_rules! impl_div_for {
+    (unsigned: [$($u:ty),* $(,)?], signed: [$($s:ty),* $(,)?]$(,)?) => {
+        $( impl_div_unsigned!($u); )*
+        $( impl_div_signed!($s); )*
+    };
+}
+
+impl_div_for! {
+    unsigned: [u8, u16, u32, u64, u128, usize],
+    signed:   [i8, i16, i32, i64, i128, isize],
+}
 
 #[cfg(test)]
 mod tests {
-    use super::DivFloor;
+    use super::*;
 
     #[test]
-    fn test_div_floor_i32() {
-        let testcases: Vec<((i32, i32), i32)> = vec![
-            ((10, 3), 3),
-            ((9, 3), 3),
-            ((8, 3), 2),
-            ((-10, 3), -4),
-            ((-9, 3), -3),
-            ((-8, 3), -3),
-            ((10, -3), -4),
-            ((9, -3), -3),
-            ((8, -3), -3),
-            ((-10, -3), 3),
-            ((-9, -3), 3),
-            ((-8, -3), 2),
-            ((0, 3), 0),
-            ((0, -3), 0),
-            ((10, 1), 10),
-            ((-10, 1), -10),
-            ((10, -1), -10),
-            ((-10, -1), 10),
+    fn test_i32() {
+        let cases: &[(i32, i32, i32)] = &[
+            //
+            // 正の数どうし
+            //
+            (0, 1, 0),
+            (1, 1, 1),
+            (1, 2, 0),
+            (2, 1, 2),
+            (2, 2, 1),
+            (3, 2, 1),
+            (5, 2, 2),
+            (6, 3, 2),
+            (7, 3, 2),
+            (10, 4, 2),
+            (100, 9, 11),
+            (i32::MAX, i32::MAX, 1),
+            (i32::MAX - 1, i32::MAX, 0),
+            //
+            // 負の数あり
+            //
+            (-4, 3, -2), // floor(-1.333..) = -2
+            (4, -3, -2), // floor(-1.333..) = -2
+            (-4, -3, 1), // floor( 1.333..) =  1
+            (-7, 3, -3),
+            (7, -3, -3),
+            (-7, -3, 2),
+            (i32::MIN, 1, i32::MIN),
+            (i32::MAX, -1, -i32::MAX),
+            (0, -5, 0),
+            (0, 5, 0),
+            (-6, 3, -2),
+            (6, -3, -2),
+            (-6, -3, 2),
         ];
 
-        for ((lhs, rhs), expected) in testcases {
-            assert_eq!(lhs.div_floor_(rhs), expected);
+        for &(a, b, exp) in cases {
+            assert_eq!(a.div_floor_(b), exp, "failed case: {a} div_floor {b}");
         }
     }
 
     #[test]
-    fn test_div_floor_u64() {
-        let testcases: Vec<((u64, u64), u64)> = vec![
-            ((10, 3), 3),
-            ((9, 3), 3),
-            ((8, 3), 2),
-            ((0, 3), 0),
-            ((10, 1), 10),
+    fn test_u64() {
+        let cases: &[(u64, u64)] = &[
+            (0, 1),
+            (1, 1),
+            (1, 2),
+            (2, 1),
+            (3, 2),
+            (u64::MAX, u64::MAX),
+            (u64::MAX - 1, u64::MAX),
+            (u64::MAX, 2),
+            (u64::MAX - 5, 3),
         ];
 
-        for ((lhs, rhs), expected) in testcases {
-            assert_eq!(lhs.div_floor_(rhs), expected);
+        for &(a, b) in cases {
+            let expected = a / b;
+            assert_eq!(a.div_floor_(b), expected, "failed case: {a} div_floor {b}");
         }
+    }
+
+    #[test]
+    fn test_smoke_all_types() {
+        assert_eq!(5u8.div_floor_(2), 2);
+        assert_eq!(5u16.div_floor_(2), 2);
+        assert_eq!(5u32.div_floor_(2), 2);
+        assert_eq!(5u64.div_floor_(2), 2);
+        assert_eq!(5u128.div_floor_(2), 2);
+        assert_eq!(5usize.div_floor_(2), 2);
+
+        assert_eq!(5i8.div_floor_(2), 2);
+        assert_eq!(5i16.div_floor_(2), 2);
+        assert_eq!(5i32.div_floor_(2), 2);
+        assert_eq!(5i64.div_floor_(2), 2);
+        assert_eq!(5i128.div_floor_(2), 2);
+        assert_eq!(5isize.div_floor_(2), 2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn u64_rhs_zero_panics() {
+        let _ = 1u64.div_floor_(0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn i32_rhs_zero_panics() {
+        let _ = 1i32.div_floor_(0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn i32_min_overflow_panics() {
+        let _ = i32::MIN.div_floor_(-1);
     }
 }
