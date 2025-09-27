4b298b993656afe5339b6753f805a967a2956aea
knrew <knrew@users.noreply.github.com>
2025-09-27 06:55:03 +0000

    deploy: 7f56d1c673ee03dcd0df57c775a88b8deac8d597

--- trait.impl/core/cmp/trait.PartialEq.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["reprol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"reprol/math/ratio/struct.Ratio.html\" title=\"struct reprol::math::ratio::Ratio\">Ratio</a>"],["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"reprol/ds/avl_tree_vec/struct.AvlTreeVec.html\" title=\"struct reprol::ds::avl_tree_vec::AvlTreeVec\">AvlTreeVec</a>&lt;T&gt;"],["impl&lt;const P: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.89.0/std/primitive.u64.html\">u64</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"reprol/math/modint/struct.ModInt.html\" title=\"struct reprol::math::modint::ModInt\">ModInt</a>&lt;P&gt;"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[1146]}
--- trait.impl/core/cmp/trait.PartialOrd.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["reprol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"reprol/math/ratio/struct.Ratio.html\" title=\"struct reprol::math::ratio::Ratio\">Ratio</a>"],["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"reprol/ds/avl_tree_vec/struct.AvlTreeVec.html\" title=\"struct reprol::ds::avl_tree_vec::AvlTreeVec\">AvlTreeVec</a>&lt;T&gt;"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[755]}
