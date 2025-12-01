8f92c90917ac8b8964a33cbaf385c61632087baf
knrew <knrew@users.noreply.github.com>
2025-12-01 01:19:55 +0000

    deploy: 0832fac964980552655d5edcae69c54062e25803

--- trait.impl/core/ops/arith/trait.RemAssign.js
+++ trait.impl/core/ops/arith/trait.RemAssign.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["reprol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/ops/arith/trait.RemAssign.html\" title=\"trait core::ops::arith::RemAssign\">RemAssign</a> for <a class=\"struct\" href=\"reprol/nonnan_float/struct.NonNanFloat.html\" title=\"struct reprol::nonnan_float::NonNanFloat\">NonNanFloat</a>"]]]]);
+    var implementors = Object.fromEntries([["reprol",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/ops/arith/trait.RemAssign.html\" title=\"trait core::ops::arith::RemAssign\">RemAssign</a> for <a class=\"struct\" href=\"reprol/nonnan_float/struct.NonNanFloat.html\" title=\"struct reprol::nonnan_float::NonNanFloat\">NonNanFloat</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
