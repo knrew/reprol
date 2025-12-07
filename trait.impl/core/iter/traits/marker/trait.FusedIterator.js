130355c29b5a82e4b85be781efb234e2a4f7a0c7
knrew <knrew@users.noreply.github.com>
2025-12-07 15:57:39 +0000

    deploy: c2c1bfadc082c0c44a08e3511dd285436fb4c6f2

---
+++ trait.impl/core/iter/traits/marker/trait.FusedIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["reprol",[["impl&lt;'a, K&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"reprol/ds/interval_map/struct.IntervalSetIter.html\" title=\"struct reprol::ds::interval_map::IntervalSetIter\">IntervalSetIter</a>&lt;'a, K&gt;"],["impl&lt;K&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"reprol/ds/interval_map/struct.IntervalSetIntoIter.html\" title=\"struct reprol::ds::interval_map::IntervalSetIntoIter\">IntervalSetIntoIter</a>&lt;K&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[784]}
---
+++ trait.impl/core/ops/range/trait.RangeBounds.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["reprol",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/ops/range/trait.RangeBounds.html\" title=\"trait core::ops::range::RangeBounds\">RangeBounds</a>&lt;T&gt; for <a class=\"struct\" href=\"reprol/ds/interval_map/struct.Interval.html\" title=\"struct reprol::ds::interval_map::Interval\">Interval</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[351]}
